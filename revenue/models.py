f9e2d6336d9798efdc294fac0194aad666ed04c5
Adan Florez <adan.florezb@gmail.com>
2021-10-22 13:33:58 -0500

    refactor: revenue model

--- revenue/models.py
+++ revenue/models.py
@@ -6,6 +6,7 @@ class Revenue(models.Model):
     amount = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     slug = models.SlugField(max_length=255, unique=True)
+    budget_date = models.DateField()
 
     def __str__(self):
         return self.subject
