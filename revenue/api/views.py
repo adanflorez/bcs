1272f5a17062d000778ba1720e18ade25411fedb
Adan Florez <adan.florezb@gmail.com>
2021-10-22 13:47:27 -0500

    refactor: filter by budget date

--- revenue/api/views.py
+++ revenue/api/views.py
@@ -17,15 +17,15 @@ class RevenueViewSet(ModelViewSet):
             if 'start_date' in self.request.query_params and 'end_date' in self.request.query_params:
                 start_date = self.request.query_params['start_date']
                 end_date = self.request.query_params['end_date']
-                return Revenue.objects.filter(created_at__gte=start_date, created_at__lte=end_date)
+                return Revenue.objects.filter(budget_date__gte=start_date, budget_date__lte=end_date)
             elif 'start_date' in self.request.query_params:
                 start_date = self.request.query_params['start_date']
                 end_date = today.strftime("%Y-%m-%d")
-                return Revenue.objects.filter(created_at__gte=start_date, created_at__lte=end_date)
+                return Revenue.objects.filter(budget_date__gte=start_date, budget_date__lte=end_date)
             elif 'end_date' in self.request.query_params:
                 start_date = today.strftime("%Y-") + "01-01"
                 end_date = self.request.query_params['end_date']
-                return Revenue.objects.filter(created_at__gte=start_date, created_at__lte=end_date)
+                return Revenue.objects.filter(budget_date__gte=start_date, budget_date__lte=end_date)
 
             return Revenue.objects.all()
         except Exception as e:
