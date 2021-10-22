5f95cda0c845c2aaf5e9083fa44e5647bb5603ed
Adan Florez <adan.florezb@gmail.com>
2021-10-21 21:06:53 -0500

    chore: migrations

--- revenue/models.py
+++ revenue/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Revenue(models.Model):
     subject = models.CharField(max_length=255)
-    amount = models.IntegerField(max_length=20, default=0)
+    amount = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     slug = models.SlugField(max_length=255, unique=True)
 
