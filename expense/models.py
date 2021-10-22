235191865fd8d92df962e47b8a9e7a4298ee9340
Adan Florez <adan.florezb@gmail.com>
2021-10-22 14:46:36 -0500

    feat: expense model

--- expense/models.py
+++ expense/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.db.models import SET_NULL
 
-# Create your models here.
+from category.models import Category
+
+
+class Expense(models.Model):
+    subject = models.CharField(max_length=255)
+    description = models.TextField()
+    amount = models.IntegerField(default=0)
+    category = models.ForeignKey(Category, on_delete=SET_NULL, null=True)
+    budget_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.subject
