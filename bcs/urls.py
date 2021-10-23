dcef56d15091b2a227a93143da35cb6102b1f572
Adan Florez <adan.florezb@gmail.com>
2021-10-22 19:48:01 -0500

    feat: budget swagger

--- bcs/urls.py
+++ bcs/urls.py
@@ -22,6 +22,7 @@ from drf_yasg import openapi
 from category.api.router import router_category
 from revenue.api.router import router_revenue
 from expense.api.router import router_expense
+from budget.api.router import router_budget
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -44,4 +45,5 @@ urlpatterns = [
    path('api/', include(router_category.urls)),
    path('api/', include(router_revenue.urls)),
    path('api/', include(router_expense.urls)),
+   path('api/', include(router_budget.urls))
 ]
